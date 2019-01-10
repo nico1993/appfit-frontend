c4f224628302598aa5f1c374dd9035d7b76f3b14
Nicolas Dona <nico.01.06.93@gmail.com>
2019-01-10 12:33:38 -0300

    Estructura de layout finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginAdminModule } from './login-admin/login-admin.module';
+import { MaterialModule } from './material.module';
+import { LayoutModule } from '@angular/cdk/layout';
 
+//Components
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { PageLayoutModule } from './components/layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    LoginAdminModule,
+    LayoutModule,
+    PageLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
